2175d5139e1abe5cc97a087f32f600aa3f2be39d
Yew Jun <tong.yewjun@setel.my>
2022-03-20 02:22:08 +0800

    fix: avoid create multiple history in short duration

--- src/pages/TodayWeather/hooks/useQueryWeather.js
+++ src/pages/TodayWeather/hooks/useQueryWeather.js
@@ -1,29 +1,37 @@
 import * as React from "react";
-import { useGetWeatherById } from "../../../queries/weatherQueries";
-import { useHistory } from "../../../providers/HistoryProvider";
+import { useGetWeatherById } from "queries/weatherQueries";
+import { useHistory } from "providers/HistoryProvider";
 
 export const useQueryWeather = (location) => {
-  const customHistoryState = useHistory();
+  const { setHistory, values } = useHistory();
   const [weatherData, setWeatherData] = React.useState(() => undefined);
 
-  const { data, refetch, isFetching } = useGetWeatherById(location.id, {
+  const { data, refetch } = useGetWeatherById(location.id, {
     enabled: false, // turned off by default due to manual refetch is needed
     refetchOnWindowFocus: false,
   });
 
   React.useEffect(() => {
     if (data) setWeatherData(data);
-  }, [data?.data, isFetching]); // trigger when refetching
+  }, [data]); // trigger when refetching
 
   React.useEffect(() => {
-    if (weatherData)
-      customHistoryState.setHistory({
+    const dateNow = Date.now();
+
+    const latestSearchAt = values[values.length - 1]?.searchAt || 0;
+    const isAllowCreateHistory = Boolean(dateNow - latestSearchAt > 500); // avoid creating history in less or qual 500 ms
+
+    if (weatherData && isAllowCreateHistory)
+      setHistory({
         id: location.id,
         country: location.country,
         city: location.city,
-        searchAt: Date.now(),
+        searchAt: dateNow,
       });
-  }, [weatherData]);
+
+    // Ignore to avoid re-render with unnecessary dependencies
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [weatherData, location]);
 
   return { data, refetch };
 };
