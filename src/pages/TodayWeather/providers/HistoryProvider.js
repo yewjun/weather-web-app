a16360f0e32ce127c86403df51077484db94c370
Yew Jun <tong.yewjun@setel.my>
2022-03-19 21:10:26 +0800

    chore: setup provider

---
+++ src/pages/TodayWeather/providers/HistoryProvider.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useCustomHistoryState } from "../hooks/useCustomHistory";
+
+const HistoryContext = React.createContext();
+
+export function HistoryProvider({ children }) {
+  const customHistoryState = useCustomHistoryState([]);
+  return (
+    <HistoryContext.Provider value={customHistoryState}>
+      {children}
+    </HistoryContext.Provider>
+  );
+}
+
+export const useHistory = () => React.useContext(HistoryContext);
