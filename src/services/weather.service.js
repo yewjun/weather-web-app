a6c4dfa527f3c7c1b9233a392842031edc0a07e6
Yew Jun <tong.yewjun@setel.my>
2022-03-19 21:10:00 +0800

    chore: setup service

---
+++ src/services/weather.service.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { config } from "../constants/config";
+
+const { openWeatherUrl, openWeatherApiKey } = config;
+
+export const getWeatherById = (id) => {
+  const url = `${openWeatherUrl}/data/2.5/weather?id=${id}&appid=${openWeatherApiKey}`;
+  return axios.get(url);
+};
